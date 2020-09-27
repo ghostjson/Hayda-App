5b47f2686d2c93171fcb58a102ed1d02fd35d162
r-aravind <aravind.ar786@gmail.com>
2020-09-28 00:13:25 +0530

    minor updates:   * health club -> health hub   * chatbot -> chat with hayda   * game page - title change   * login page - removed hello   * typo fixes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,36 +1,35 @@
-import {Component} from '@angular/core';
-import {LinkCardInterface} from "../../components/link-card/link-card.interface";
-import {NavController} from "@ionic/angular";
+import { Component } from "@angular/core";
+import { LinkCardInterface } from "../../components/link-card/link-card.interface";
+import { NavController } from "@ionic/angular";
 
 @Component({
-    selector: 'app-home',
-    templateUrl:  'home.page.html',
-    styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  blogs: LinkCardInterface[];
 
-    blogs: LinkCardInterface[];
+  constructor(private navCtrl: NavController) {
+    this.blogs = [
+      {
+        background: "../../../assets/images/placeholders/blog_mock_image.jpg",
+        title: "BlogTitle 1",
+        url: "#",
+      },
+      {
+        background: "../../../assets/images/placeholders/blog_mock_image2.jpg",
+        title: "BlogTitle 3",
+        url: "#",
+      },
+    ];
+  }
 
-    constructor(private navCtrl: NavController) {
-        this.blogs = [
-            {
-                background: '../../../assets/images/placeholders/blog_mock_image.jpg',
-                title: 'BlogTitle 1',
-                url: '#'
-            },
-            {
-                background: '../../../assets/images/placeholders/blog_mock_image2.jpg',
-                title: 'BlogTitle 3',
-                url: '#'
-            }
-        ];
-    }
+  async goToWorkout() {
+    await this.navCtrl.navigateForward("workout");
+  }
 
-    async goToWorkout() {
-        await this.navCtrl.navigateForward('workout');
-    }
-
-    async goToBlog() {
-        await this.navCtrl.navigateForward('blog');
-    }
+  async goToBlog() {
+    await this.navCtrl.navigateForward("blog");
+  }
 }
